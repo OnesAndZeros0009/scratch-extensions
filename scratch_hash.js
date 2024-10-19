f2b554e89c0bc05d76ef98cc165fd014f6ffe54c
OnesAndZeros0009 <121014628+OnesAndZeros0009@users.noreply.github.com>
2024-10-19 15:54:45 +1100

    Update scratch_hash.js

--- scratch_hash.js
+++ scratch_hash.js
@@ -1,4 +1,4 @@
-class ScratchHash {
+class ScratchPasswordUtils{
     constructor() {
     }
     
@@ -8,7 +8,7 @@ class ScratchHash {
             "name": "hash",
             "blocks": [
                         {
-                            "opcode": "fetchURL",
+                            "opcode": "hash",
                             "blockType": "reporter",
                             "text": "convert [input] to hash",
                             "arguments": {
@@ -18,15 +18,41 @@ class ScratchHash {
                                 },
                             }
                         },
+						{
+                            "opcode": "salt",
+                            "blockType": "reporter",
+                            "text": "salt [input] on side [side] [salt] times",
+                            "arguments": {
+                                "input": {
+                                    "type": "string",
+                                    "defaultValue": "123"
+                                },
+								"side": {
+                                    "type": "number",
+                                    "menu": "sides"
+                                },
+								"salt": {
+                                    "type": "string",
+                                    "defaultValue": "50"
+                                },
+                            }
+                        },
                 ],
+				"menus": {
+					"sides": [{text:"left",value:1}, {text:"right",value:2}, {text:"both",value:3}],
+            }
         };
     }
     
-	async fetchURL({input}) {
+	async hash({input}) {
 		const hashBuffer = await crypto.subtle.digest('SHA-1', new TextEncoder().encode(input));
 		const hashArray = Array.from(new Uint8Array(hashBuffer));
 		const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
 		return hashHex;
+	}
+	
+	salt({input, side, salt}) {
+		return 0
 	}
 }
 
