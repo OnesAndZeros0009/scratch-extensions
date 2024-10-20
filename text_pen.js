8613a5da25495e12db08d5d9caf1eb1c3b349f02
OnesAndZeros0009 <121014628+OnesAndZeros0009@users.noreply.github.com>
2024-10-20 19:10:48 +1100

    Update text_pen.js

--- text_pen.js
+++ text_pen.js
@@ -1,78 +1,25 @@
-class ScratchHash {
+class TextPen {
     constructor() {
     }
     
     getInfo() {
         return {
-            "id": "Hash",
-            "name": "hash",
+            "id": "Text_pen",
+            "name": "Text Pen",
             "blocks": [
                         {
-                            "opcode": "hash",
-                            "blockType": "reporter",
-                            "text": "convert [input] to hash",
+                            "opcode": "text",
+                            "blockType": "command",
+                            "text": "draw text [text] at current pos",
                             "arguments": {
-                                "input": {
+                                "text": {
                                     "type": "string",
-                                    "defaultValue": "banana"
-                                },
+                                    "defaultValue": "apple"
+                                }
                             }
-                        },
-						                        {
-                            "opcode": "salt",
-                            "blockType": "reporter",
-                            "text": "salt [input] [times] times on side [sides]",
-                            "arguments": {
-                                "input": {
-                                    "type": "string",
-                                    "defaultValue": ""
-                                },
-								"times": {
-                                    "type": "number",
-                                    "defaultValue": "50"
-                                },
-								"sides": {
-                                    "type": "number",
-                                    "menu": "sides"
-                                },
-                            }
-                        },
-                ],
-			"menus": {
-                "sides": [{text:"both",value:0}, {text:"left",value:1}, {text:"right",value:2}],
-            }            
+                        }
+                ]         
         };
     }
-    
-	async hash({input}) {
-		const hashBuffer = await crypto.subtle.digest('SHA-1', new TextEncoder().encode(input));
-		const hashArray = Array.from(new Uint8Array(hashBuffer));
-		const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-		return hashHex;
-	}
-	salt({input, times, sides}) {
-		function makerand(length) {
-			let result = '';
-			const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-			const charactersLength = characters.length;
-			let counter = 0;
-			while (counter < length) {
-			  result += characters.charAt(Math.floor(Math.random() * charactersLength));
-			  counter += 1;
-			}
-			return result;
-		}
-			
-		var text = input
-		if (sides == 0){
-			text = makerand(times) + text;
-			text += makerand(times)
-		}else if (sides == 1){
-			text = makerand(times) + text;
-		} else {
-			text += makerand(times)
-		}
-		return text
-	}
 }
-Scratch.extensions.register(new ScratchHash())
+Scratch.extensions.register(new Textpen())
